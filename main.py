9d19a642d07c435fa62d13b91bc3fa79235a7940
ethanrasmussen <itseraz@gmail.com>
2021-04-23 12:22:46 -0500

    restructure menu to func's

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import bt_discover as btd
 import servo_op as sop
 
 
-# cmd line menu
-print("----- MENU: -----")
-print(" 1. Start Script")
-print(" 2. Edit Whitelist")
-print("-----------------")
-menu_opt = input("Please enter 1 or 2: ")
-if menu_opt == "1":
+def main_menu():
+    # cmd line menu
+    print("----- MENU: -----")
+    print(" 1. Start Script")
+    print(" 2. Edit Whitelist")
+    print("-----------------")
+    menu_opt = input("Please enter 1 or 2: ")
+    if menu_opt == "1":
+        run_script()
+    elif menu_opt == "2":
+        edit_whitelist()
+    else:
+        print("Improper input value. Please enter any key to try again.")
+        input()
+        print("\n\n\n\n\n\n")
+        main_menu()
+
+def edit_whitelist():
     pass
-elif menu_opt == "2":
+def run_script():
     pass
-else:
-    print("Cannot process input. Exiting program...")
-    exit()
