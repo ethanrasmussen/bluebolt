88c01686a0794fffed75dee883118fd8144697f7
ethanrasmussen <itseraz@gmail.com>
2021-04-23 12:20:22 -0500

    menu base

---
+++ main.py
@@ -0,0 +1,17 @@
+import bt_discover as btd
+import servo_op as sop
+
+
+# cmd line menu
+print("----- MENU: -----")
+print(" 1. Start Script")
+print(" 2. Edit Whitelist")
+print("-----------------")
+menu_opt = input("Please enter 1 or 2: ")
+if menu_opt == "1":
+    pass
+elif menu_opt == "2":
+    pass
+else:
+    print("Cannot process input. Exiting program...")
+    exit()
