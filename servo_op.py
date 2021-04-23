d2d4d149c32e7e96fa7175a71a6b04644346af31
ethanrasmussen <itseraz@gmail.com>
2021-04-23 17:04:31 -0500

    servo positioning via duty cycles

--- servo_op.py
+++ servo_op.py
@@ -1,4 +1,4 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 import json
 
@@ -6,24 +6,27 @@ import json
 # get GPIO pin number from config
 with open('config.json') as f:
     data = json.load(f)
-pin = int(data['servo_pin'])
+pin = data['servo_pin']
+open_pos = data['opened_pos_dutycycle']
+close_pos = data['closed_pos_dutycycle']
 
 # set up GPIO for movement
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(pin, GPIO.OUT)
-# pwm = GPIO.pwm(pin, 50)
-# pwm.start(0)
-# TODO: set intial position for full turn
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+pwm = GPIO.pwm(pin, 50)
+pwm.start(0)
 
 
 # turn to open the deadbolt
 def open_bolt():
-    pass
+    pwm.ChangeDutyCycle(open_pos)
+    sleep(1)
 
 # turn to close the deadbolt
 def close_bolt():
-    pass
+    pwm.ChangeDutyCycle(close_pos)
+    sleep(1)
 
 # stop servo
 def stop():
-    pass
+    pwm.stop()
